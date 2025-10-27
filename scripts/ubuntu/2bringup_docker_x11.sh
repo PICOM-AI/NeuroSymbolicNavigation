51ed6e397105f13ecb2d8dd8c1869b9a1cbf4b4e
Duc Manh Nguyen <duc.manh.nguyen@tu-berlin.de>
2025-10-27 17:25:52 +0100

    update cmd

--- scripts/ubuntu/2bringup_docker_x11.sh
+++ scripts/ubuntu/2bringup_docker_x11.sh
@@ -18,4 +18,4 @@ docker run --rm -it \
 --device /dev/dri \
 -v "$(pwd)/solver:/workspace/solver" \
 "$IMAGE" \
-bash -lc 'exec ros2 launch turtlebot4_ignition_bringup turtlebot4_ignition.launch.py localization:=true world:=maze map:=maze/maze.yaml slam:=true'
+bash -lc 'exec ros2 launch turtlebot4_ignition_bringup turtlebot4_ignition.launch.py localization:=true world:=maze map:=maze/maze.yaml slam:=true rviz:=true'
