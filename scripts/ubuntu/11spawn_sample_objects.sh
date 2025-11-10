7ae126e1dfd681cd0deaed99cd29ef2cfcc5ed96
Duc Manh Nguyen <duc.manh.nguyen@tu-berlin.de>
2025-11-10 03:20:54 +0100

    update handson 5 and 6

--- scripts/ubuntu/11spawn_sample_objects.sh
+++ scripts/ubuntu/11spawn_sample_objects.sh
@@ -14,3 +14,7 @@ ros2 run ros_gz_sim create -world maze -file fire_hydrant/model.sdf -name firehy
 docker exec -it turtlebot4_sim bash -c "source /opt/ros/humble/setup.bash &&
 cd /workspace/solver && 
 ros2 run ros_gz_sim create -world maze -file construction_cone/model.sdf -name constructioncone1 -x 2 -y -1 -z 0.1"
+
+docker exec -it turtlebot4_sim bash -c "source /opt/ros/humble/setup.bash &&
+cd /workspace/solver && 
+ros2 run ros_gz_sim create -world maze -file cardboard_box/model.sdf -name cardboardbox1 -x 1 -y 0 -z 0.1"
